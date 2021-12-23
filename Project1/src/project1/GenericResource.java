8ad37b101e22084219e4ed6e200aee3585e00c3d
sgummaraju <sgummaraju@ecovues.com>
2021-12-23 11:43:01 +0530

    Source code

---
+++ Project1/src/project1/GenericResource.java
@@ -0,0 +1,18 @@
+package project1;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+
+@Path("project1")
+public class GenericResource {
+    public GenericResource() {
+    }
+
+    @GET
+    @Path("/validate")
+    public String getData() {
+
+        return "Working";
+    }
+}
